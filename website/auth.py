f3379bd51b9d52c8ec9115f8df2943044b074869
OmarJ <omarjandali93@gmail.com>
2021-12-01 13:27:09 -0800

    added the initial templatees for user login, signup, logout, and home. Rendered the home page. NEXT ->  connecting the templates and starting user authentication

--- website/auth.py
+++ website/auth.py
@@ -2,4 +2,14 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('/login')
+def login():
+  return "<h1> Login </h1>"
 
+@auth.route('/logout')
+def logout():
+  return "<h1> logout </h1>"
+
+@auth.route('/signup')
+def signup():
+  return "<h1> signup </h1>"
